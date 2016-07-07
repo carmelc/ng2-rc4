c9d0a26d1256a96e9fa45c68d5ae2fce4efb4466
Carmel Cohen <carmelc@wix.com>
2016-07-07 17:07:53 +0300

    feat(Todo App): new ng-model

--- app/components/todo-input.component.ts
+++ app/components/todo-input.component.ts
@@ -2,15 +2,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'todo-input',
   template:
-    `<label for="todo-input">Todo: </label><input id="todo-input" type='text' #todoInput [value]="defaultValue" (input)="defaultValue = todoInput.value"/>
-    <button type="button" (click)="onAdd(todoInput.value)">Add</button>
-    <div>{{defaultValue}}</div>
+    `<label for="todo-input">Todo: </label><input id="todo-input" type='text' [(ngModel)]="value"/>
+    <button type="button" (click)="onAdd()">Add</button>
+    <div>{{value}}</div>
     `
 })
 export class TodoInputComponent {
-  defaultValue = 'please provide a value';
+  value = 'please provide a value';
 
-  onAdd(value) {
-    console.log('default is:\n\'', this.defaultValue, '\'\nnew is:\n\'', value, '\'');
+  onAdd() {
+    console.log('value is:\'', this.value, '\'');
   }
 }
