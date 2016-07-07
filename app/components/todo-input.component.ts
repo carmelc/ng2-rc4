5b9593bfaec8aaac9dc8bc154b7bbc34d581bc43
Carmel Cohen <carmelc@wix.com>
2016-07-07 16:13:10 +0300

    feat(Todo App): two ways binding using (input)

--- app/components/todo-input.component.ts
+++ app/components/todo-input.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'todo-input',
   template:
-    `<label for="todo-input">Todo: </label><input id="todo-input" type='text' #todoInput [value]="defaultValue"/>
+    `<label for="todo-input">Todo: </label><input id="todo-input" type='text' #todoInput [value]="defaultValue" (input)="defaultValue = todoInput.value"/>
     <button type="button" (click)="onAdd(todoInput.value)">Add</button>
     `
 })
